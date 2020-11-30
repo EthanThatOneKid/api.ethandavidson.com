7c2850313b768e99fa4f1233a05f7a094efdf0f6
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-11-29 22:59:40 -0800

    Update constants.ts

--- shared/constants.ts
+++ shared/constants.ts
@@ -2,10 +2,20 @@ import { config } from "../deps.ts";
 
 const env: Record<string, string> = config();
 
-export const PORT = env["PORT"],
-  GITHUB_HANDLE = env["GITHUB_HANDLE"],
-  TWITTER_HANDLE = env["TWITTER_HANDLE"],
-  TWITTER_BEARER_TOKEN = env["TWITTER_BEARER_TOKEN"];
+export const PORT = String(env["PORT"] || Deno.env.get("PORT"));
+
+export const GITHUB_HANDLE = String(
+  env["GITHUB_HANDLE"] || Deno.env.get("GITHUB_HANDLE"),
+);
+
+export const TWITTER_HANDLE = String(
+  env["TWITTER_HANDLE"] || Deno.env.get("TWITTER_HANDLE"),
+);
+
+export const TWITTER_BEARER_TOKEN = String(
+  env["TWITTER_BEARER_TOKEN"] ||
+    Deno.env.get("TWITTER_BEARER_TOKEN"),
+);
 
 for (
   const [name, value] of Object.entries(
@@ -18,7 +28,7 @@ for (
   )
 ) {
   console.log({ name, value });
-  if (value === undefined || value.length === 0) {
+  if (value === "undefined" || value.length === 0) {
     throw new Error(`Expected secret ${name} in '.env' file.`);
   }
 }
