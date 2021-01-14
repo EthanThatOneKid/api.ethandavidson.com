1a5e216b734afedd2de9e7dcaf0ebc817f0b1e04
Code Ninjas Garden Grove <68670709+codeninjasgg@users.noreply.github.com>
2021-01-13 18:20:28 -0800

    Update mod.ts

--- routes/mod.ts
+++ routes/mod.ts
@@ -1,9 +1,15 @@
 import { Router } from "../deps.ts";
 import type { Context } from "../deps.ts";
+
 import aboutRoute from "./about/mod.ts";
+import experienceRoute from "./experience/mod.ts";
 
 const indexRoute = new Router().get("/", (ctx: Context) => {
   ctx.response.body = { hello: "world" };
 });
 
-export const routers: Router[] = [indexRoute, aboutRoute];
+export const routers: Router[] = [
+  indexRoute,
+  aboutRoute,
+  experienceRoute
+];
