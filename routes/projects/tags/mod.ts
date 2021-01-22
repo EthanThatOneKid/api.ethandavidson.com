ab2f03e0c46e0d0df71f32dd28dba9b6567489f4
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-01-22 01:27:17 -0800

    Update mod.ts

--- routes/projects/tags/mod.ts
+++ routes/projects/tags/mod.ts
@@ -1,6 +1,6 @@
 import { data } from "./data.ts";
 
-const technologies = ["qb64", "quickbasic"] as const;
+const technologies = ["qb64", "quickbasic", "sapper"] as const;
 
 export type ProjectTag = typeof data[number] | typeof technologies[number];
 export const tags = [...data];
