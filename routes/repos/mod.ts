540c6963eff01b07fcb303fd94c206c50b69d6c3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-02-11 14:05:12 -0800

    Update mod.ts

--- routes/repos/mod.ts
+++ routes/repos/mod.ts
@@ -1,19 +1,23 @@
 import { Router } from "../../deps.ts";
 import type { RouterContext } from "../../deps.ts";
-import { getRepo } from "./github.ts";
+// import { getRepo } from "./github.ts";
 
-const reposRoute = new Router()
-  .get("/repos/:repo", async (ctx: RouterContext) => {
-    const { repo } = ctx.params;
-    console.log({ repo });
-    // console.log({ ctx });
-    if (repo === undefined) {
-      ctx.response.body = {};
-    } else {
-      const payload = await getRepo([repo]);
-      // console.log({ payload });
-      ctx.response.body = { ...payload };
-    }
-  });
+const reposRoute = new Router();
+// .get("/repos/:repo", async (ctx: RouterContext) => {
+//   const { repo } = ctx.params;
+//   console.log({ repo });
+//   // console.log({ ctx });
+//   if (repo === undefined) {
+//     ctx.response.body = {};
+//   } else {
+//     const payload = await getRepo([repo]);
+//     // console.log({ payload });
+//     ctx.response.body = { ...payload };
+//   }
+// });
+
+reposRoute.use("repos", async (ctx: RouterContext) => {
+  console.log({ ctx });
+});
 
 export default reposRoute;
