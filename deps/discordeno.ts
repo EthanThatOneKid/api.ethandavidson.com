823bde2d8699276713be77e892987dd2865ae97b
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-01 15:42:11 -0800

    Added some discord bot stuff

---
+++ deps/discordeno.ts
@@ -0,0 +1,1 @@
+export { cache } from "https://deno.land/x/discordeno@10.4.0/src/util/cache.ts";
---
+++ lib/discord-client/mod.ts
@@ -0,0 +1,6 @@
+// To-do:
+// - https://discordeno.mod.land/stepbystep/createbot.html
+
+// Features:
+// - Gets Ethan's latest fetched status from cache.
+// - Send Ethan messages from `POST /contact` requests.
