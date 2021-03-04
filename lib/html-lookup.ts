5c1cf95188363eba9c828a3b80f72c6c17cbd881
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-04 13:03:07 -0800

    Added logic for the github profile scraping algorithm

--- lib/html-lookup.ts
+++ lib/html-lookup.ts
@@ -4,7 +4,7 @@ import {
 } from "https://deno.land/x/deno_dom@v0.1.6-alpha/deno-dom-wasm.ts";
 
 export const htmlLookup = (html: string) => {
-  const doc = new DOMParser().parseFromString(html)!;
+  const doc = new DOMParser().parseFromString(html, "text/html")!;
   return (querySelector: string) => {
     return doc.querySelector(querySelector);
   };
--- lib/scrape-github-profile.ts
+++ lib/scrape-github-profile.ts
@@ -1,22 +1,48 @@
 import htmlLookup from "./html-lookup.ts";
-import { GITHUB_HANDLE } from "./constants";
+import { GITHUB_HANDLE } from "./constants.ts";
 import type { GitHubProfileScrapeResult } from "./types.ts";
+
+// TODO:
+// - Integrate cache system, like for the repos route.
+
+const selectors = {
+  bio:
+    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-3.mb-4.mb-md-0 > div > div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4 > div",
+  followers:
+    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-3.mb-4.mb-md-0 > div > div.d-flex.flex-column > div.js-profile-editable-area.d-flex.flex-column.d-md-block > div.flex-order-1.flex-md-order-none.mt-2.mt-md-0 > div > a:nth-child(1) > span",
+  statusEmoji:
+    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-3.mb-4.mb-md-0 > div > div.clearfix.d-flex.d-md-block.flex-items-center.mb-4.mb-md-0 > div.position-relative.d-inline-block.col-2.col-md-12.mr-3.mr-md-0.flex-shrink-0 > div > div > div > details > summary > div > div.f6.lh-condensed.user-status-header.d-inline-flex.user-status-emoji-only-header.circle > div > div > g-emoji",
+  statusMessage:
+    "#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-3.mb-4.mb-md-0 > div > div.clearfix.d-flex.d-md-block.flex-items-center.mb-4.mb-md-0 > div.position-relative.d-inline-block.col-2.col-md-12.mr-3.mr-md-0.flex-shrink-0 > div > div > div > details > summary > div > div.ws-normal.user-status-message-wrapper.f6.min-width-0 > div > span > div",
+} as const;
 
 export const scrapeGitHubProfile = async (): Promise<
   GitHubProfileScrapeResult
 > => {
   const res = await fetch(`https://github.com/${GITHUB_HANDLE}/`);
   const html = await res.text();
-  const result = htmlLookup(html);
+  const querySelector = htmlLookup(html);
   const payload: GitHubProfileScrapeResult = {
-    bio: "string",
-    followers: 0,
-    contributionActivity: [],
+    bio: querySelector(selectors.bio)?.textContent ?? "",
+    followers: Number(querySelector(selectors.followers)?.textContent ?? 0),
+    contributionActivity: parseContributionActivity(),
     userStatus: {
-      emoji: "string",
-      message: "string",
+      emoji: querySelector(selectors.statusEmoji)?.textContent ?? "",
+      message: querySelector(selectors.statusMessage)?.textContent ?? "",
     },
-    pinnedRepos: [],
+    pinnedRepos: parsePinnedRepos(),
   };
   return payload;
 };
+
+const parsePinnedRepos = () => {
+  // TODO
+  return [];
+};
+
+const parseContributionActivity = () => {
+  // TODO
+  return [];
+};
+
+export default scrapeGitHubProfile;
